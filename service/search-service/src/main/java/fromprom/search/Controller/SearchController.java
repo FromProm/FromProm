35e4b6ed261f6e7ceffc9e75feeefec436c4686d
uijin00 <tlsdmlwls21@naver.com>
2026-01-06 15:16:27 +0900

    미사용 import 삭제

--- service/search-service/src/main/java/fromprom/search/Controller/SearchController.java
+++ service/search-service/src/main/java/fromprom/search/Controller/SearchController.java
@@ -3,7 +3,6 @@ package fromprom.search.Controller;
 import fromprom.search.DTO.PromptDocument;
 import fromprom.search.Service.SearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

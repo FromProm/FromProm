63e1e49d76d1cd4ae399c6f553b64386e6e82409
Jieun55 <ohje7530@naver.com>
2026-01-19 16:22:21 +0900

    ci test

--- service/auth-service/src/main/java/FromProm/user_service/Configuration/SecurityConfig.java
+++ service/auth-service/src/main/java/FromProm/user_service/Configuration/SecurityConfig.java
@@ -21,11 +21,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // POST, PATCH 테스트를 위해 필수!
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/healthy").permitAll()
-                        .anyRequest().permitAll() // 일단 모든 API를 인증 없이 허용 (테스트용)
+                        .anyRequest().permitAll()
                 );
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
-                
+
         return http.build();
     }
 
